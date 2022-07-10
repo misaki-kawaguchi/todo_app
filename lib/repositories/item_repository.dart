2603b12bb142048bb1753c85e5e95ba3e39e3dbd
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-10 20:09:01 +0900

    repositoryにcreateItemを追加

--- lib/repositories/item_repository.dart
+++ lib/repositories/item_repository.dart
@@ -1,5 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:todo_app/models/item_model.dart';
+
+// overrideで値を書き換えられるabstractクラス
+abstract class BaseItemRepository {
+  Future<String> createItem({required Item item});
+}
 
 // Firestoreのインスタンスを取得するProvider
 final firebaseFirestoreProvider =
@@ -9,8 +15,24 @@ final firebaseFirestoreProvider =
 final itemRepositoryProvider =
     Provider<ItemRepository>((ref) => ItemRepository(ref.read));
 
-class ItemRepository {
+class ItemRepository implements BaseItemRepository {
   const ItemRepository(this._read);
   // 外部からProviderを取得可能にする
   final Reader _read;
+
+  // addメソッドで値を追加する
+  @override
+  Future<String> createItem({required Item item}) async {
+    // listsコレクションにitemを追加
+    try {
+      final docRef =
+          await _read(firebaseFirestoreProvider).collection('lists').add(
+                item.toDocument(),
+              );
+      // ドキュメントのidを返す
+      return docRef.id;
+    } catch (e) {
+      throw e.toString();
+    }
+  }
 }
