2802f59dc363c57c9a5f60398ed507d2bea71c7b
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-10 21:28:56 +0900

    itemを追加できるようにする

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo_app/ui/pages/item_list_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
--- lib/ui/widgets/add_item_dialog.dart
+++ lib/ui/widgets/add_item_dialog.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo_app/models/item_model.dart';
+import 'package:todo_app/view_models/item_list_view_model.dart';
 
-class AddItemDialog extends StatelessWidget {
+class AddItemDialog extends HookConsumerWidget {
   const AddItemDialog({
     Key? key,
     required this.item,
@@ -18,9 +21,37 @@ class AddItemDialog extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final textController = useTextEditingController(text: item.title);
+    final itemListNotifier = ref.watch(itemListProvider.notifier);
     return Dialog(
-      child: Text('ダミー'),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration: const InputDecoration(hintText: 'タイトル'),
+            ),
+            const SizedBox(height: 12.0),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor,
+                ),
+                onPressed: () {
+                  itemListNotifier.addItem(title: textController.text.trim());
+                  Navigator.pop(context);
+                },
+                child: const Text('追加'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
