f67a9ab4b686e224babd86b9ecad67c7615b98d1
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-10 19:50:04 +0900

    itemRepositoryProviderとItemRepositoryクラスを作成

--- lib/repositories/item_repository.dart
+++ lib/repositories/item_repository.dart
@@ -2,4 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Firestoreのインスタンスを取得するProvider
-final firebaseFirestoreProvider = Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
+final firebaseFirestoreProvider =
+    Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
+
+// ItemRepositoryの内容を管理するProvider
+final itemRepositoryProvider =
+    Provider<ItemRepository>((ref) => ItemRepository(ref.read));
+
+class ItemRepository {
+  const ItemRepository(this._read);
+  // 外部からProviderを取得可能にする
+  final Reader _read;
+}
