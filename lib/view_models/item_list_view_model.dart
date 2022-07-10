771e36965056a77bd9fd8df32483545fa38d099c
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2022-07-10 21:02:36 +0900

    viewmodelにaddItemメソッドを追加

--- lib/view_models/item_list_view_model.dart
+++ lib/view_models/item_list_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo_app/models/item_model.dart';
+import 'package:todo_app/repositories/item_repository.dart';
 
 // ItemListNotifierの内容を管理するProviderを作成
 final itemListProvider =
@@ -13,4 +14,29 @@ class ItemListNotifier extends StateNotifier<AsyncValue<List<Item>>> {
 
   // 外部からProviderを取得可能にする
   final Reader _read;
+
+  // Itemを追加
+  Future<void> addItem(
+      {required String title, bool isCompleted = false}) async {
+    try {
+      final item = Item(
+        title: title,
+        isCompleted: isCompleted,
+        createdAt: DateTime.now(),
+      );
+      // ドキュメントのid
+      final itemId = await _read(itemRepositoryProvider).createItem(item: item);
+      // item一覧にitemを追加（itemモデルのidにドキュメントのidにコピーする）
+      state.whenData(
+        (items) => state = AsyncValue.data(
+          items
+            ..add(
+              item.copyWith(id: itemId),
+            ),
+        ),
+      );
+    } catch (e) {
+      throw e.toString();
+    }
+  }
 }
